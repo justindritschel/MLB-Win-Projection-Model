71232dda5d3030cbcac11587bcd8ab3e23981415
justindritschel <144080111+justindritschel@users.noreply.github.com>
2024-03-05 22:50:20 -0500

    Some changes to the functions

--- Implied Probability and Expected Value Functions.R
+++ Implied Probability and Expected Value Functions.R
@@ -6,11 +6,11 @@ odds_to_prob <- function(odds) {
   } else {
     imp_prob <<- 100 / (abs(odds) + 100)
   }
-  return(imp_prob)
+  paste("Implied Probability of the Sportsbook odds:", imp_prob)
 }
 
 
 expected_value_of_bet <- function(potential_winnings = bet / 1 - imp_prob, model_prob, bet = 100, imp_prob){
-  return ((potential_winnings * model_prob) - (bet * imp_prob))
+  paste("Expected value of your bet",((potential_winnings * model_prob) - (bet * imp_prob)))
 }
 #Where model_prob is defined as the model's probability of winning x number of games
